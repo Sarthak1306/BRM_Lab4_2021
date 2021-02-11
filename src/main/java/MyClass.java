5bc750fb67848f2232d7ca76b69f27015276216a
Sarthak1306 <sarthak13062001@gmail.com>
2021-02-11 10:58:42 +0530

    Added L2

--- src/main/java/MyClass.java
+++ src/main/java/MyClass.java
@@ -4,6 +4,7 @@ public class MyClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Github & Jenkins");
+		System.out.println("Testing Jenkins Integration with Maven");
 	}
 
 }
